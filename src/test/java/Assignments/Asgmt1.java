a87b6c72d582491dd9dc6c7a577d8292a5e4680f
PanditUtkarsh <utkp200@gmail.com>
2024-02-22 01:51:26 +0530

    assignment

---
+++ src/test/java/Assignments/Asgmt1.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Asgmt1 {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://admin-demo.nopcommerce.com");
+		
+	} 
+	
+}
