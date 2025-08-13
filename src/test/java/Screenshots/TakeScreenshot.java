5298d676102a8b3024db98028e1e89b59eec0021
Utkp5 <utkp200@gmail.com>
2025-08-13 11:59:33 +0530

    Screenshot

--- src/test/java/Screenshots/TakeScreenshot.java
+++ src/test/java/Screenshots/TakeScreenshot.java
@@ -4,9 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.io.FileHandler;
 
@@ -20,14 +22,33 @@ public class TakeScreenshot {
 		driver.get("https://demo.nopcommerce.com/");
 		driver.manage().window().maximize();
 
+		//refresh to see the ss in folder
 		
-		// Capture full page screenshot - selenium 3 & 4
+		// Way 1  Capture full page screenshot - selenium 3 & 4
 		 TakesScreenshot ts = (TakesScreenshot)driver; 
 		 File from = ts.getScreenshotAs(OutputType.FILE); 
 		 File to = new File("D:\\Selenium-Udemy\\Screenshots\\fullpage.png");
 		 FileHandler.copy(from, to);	
 		 
-		 //refresh to see the ss in folder
+		 
+		 
+		// Way 2 Capture full page screenshot
+//		 File sourceFile = ts.getScreenshotAs(OutputType.FILE);
+//		 File targetFile = new File(System.getProperty("user.dir")+"\\Screenshots\\fullpage.png");
+//		 sourceFile.renameTo(targetFile);
+		 
+		 
+		 
+		// Capture the screenshot of specific section
+		 
+		 WebElement section = driver.findElement(By.xpath("//div[@class='product-grid home-page-product-grid']"));
+		 
+		 File sourceFile = section.getScreenshotAs(OutputType.FILE);
+		 File targetFile = new File(System.getProperty("user.dir")+"\\Screenshots\\featued.png");
+		 sourceFile.renameTo(targetFile);
+	
+		 
+		 driver.close();
 	}
 
 }
